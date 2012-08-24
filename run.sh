92c7700b00ee62b6f890b7c128e83ff26dd16604
Ivan Ivanov <ivan.vladimirov.ivanov@gmail.com>
2012-08-24 12:37:23 +0300

    Adding a README file and cleaning up unused junk.

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-
-#bin/main
-
-bin/main --from_user 0   --to_user 100 --recommendation_file r1.txt
-
-#echo 'starting processes'
-#bin/main --from_user 0   --to_user 100 --recommendation_file r1.txt &
-#bin/main --from_user 101 --to_user 200 --recommendation_file r2.txt &
+bin/main --recommendation_file recommendations.txt --number_of_threads 1
 
